ddd44eb60d1c8d70d6c88ebe88206ada1be0d62b
31awa <3160709820@qq.com>
2025-07-26 15:30:28 +0800

    添加匠魂与TFC的矿物支持,调整config

---
+++ kubejs/server_scripts/tinkjs.js
@@ -0,0 +1,630 @@
+//融化时间5mb=1秒
+ServerEvents.recipes(event => 
+event.custom({//定义在炉子里融化
+"type": "tconstruct:melting",
+"ingredient": {//所需物品
+"item": "tfc:raw_iron_bloom" //你输入的物品
+},
+"result": {//结果
+"amount": 100,//90mb
+"fluid": "tconstruct:molten_iron"//你定义的流体
+},
+"temperature": 800,//温度 800是常规燃料 1000是岩浆 1500是烈焰血
+"time": 80//时间 4是1秒
+}),
+)
+
+ServerEvents.recipes(event => 
+event.custom({//定义在炉子里融化
+"type": "tconstruct:melting",
+"ingredient": {//所需物品
+"item": "tfc:refined_iron_bloom" //你输入的物品
+},
+"result": {//结果
+"amount": 100,//90mb
+"fluid": "tconstruct:molten_iron"//你定义的流体
+},
+"temperature": 800,//温度 800是常规燃料 1000是岩浆 1500是烈焰血
+"time": 80//时间 4是1秒
+}),
+)
+
+ServerEvents.recipes(event => 
+event.custom({//定义在炉子里融化
+"type": "tconstruct:melting",
+"ingredient": {//所需物品
+"item": "tfc:ore/poor_native_copper" //你输入的物品
+},
+"result": {//结果
+"amount": 15,//90mb
+"fluid": "tconstruct:molten_copper"//你定义的流体
+},
+"temperature": 800,//温度 800是常规燃料 1000是岩浆 1500是烈焰血
+"time": 24//时间 4是1秒
+}),
+)
+
+ServerEvents.recipes(event => 
+event.custom({//定义在炉子里融化
+"type": "tconstruct:melting",
+"ingredient": {//所需物品
+"item": "tfc:ore/rich_native_copper" //你输入的物品
+},
+"result": {//结果
+"amount": 35,//90mb
+"fluid": "tconstruct:molten_copper"//你定义的流体
+},
+"temperature": 800,//温度 800是常规燃料 1000是岩浆 1500是烈焰血
+"time": 28//时间 4是1秒
+}),
+)
+
+ServerEvents.recipes(event => 
+event.custom({//定义在炉子里融化
+"type": "tconstruct:melting",
+"ingredient": {//所需物品
+"item": "tfc:ore/normal_native_copper" //你输入的物品
+},
+"result": {//结果
+"amount": 15,//90mb
+"fluid": "tconstruct:molten_copper"//你定义的流体
+},
+"temperature": 800,//温度 800是常规燃料 1000是岩浆 1500是烈焰血
+"time": 12//时间 4是1秒
+}),
+)
+
+ServerEvents.recipes(event => 
+event.custom({//定义在炉子里融化
+"type": "tconstruct:melting",
+"ingredient": {//所需物品
+"item": "tfc:ore/small_native_copper" //你输入的物品
+},
+"result": {//结果
+"amount": 10,//90mb
+"fluid": "tconstruct:molten_copper"//你定义的流体
+},
+"temperature": 800,//温度 800是常规燃料 1000是岩浆 1500是烈焰血
+"time": 8//时间 4是1秒
+}),
+)
+
+ServerEvents.recipes(event => 
+event.custom({//定义在炉子里融化
+"type": "tconstruct:melting",
+"ingredient": {//所需物品
+"item": "tfc:ore/poor_native_gold" //你输入的物品
+},
+"result": {//结果
+"amount": 15,//90mb
+"fluid": "tconstruct:molten_gold"//你定义的流体
+},
+"temperature": 800,//温度 800是常规燃料 1000是岩浆 1500是烈焰血
+"time": 12//时间 4是1秒
+}),
+)
+
+ServerEvents.recipes(event => 
+event.custom({//定义在炉子里融化
+"type": "tconstruct:melting",
+"ingredient": {//所需物品
+"item": "tfc:ore/small_native_gold" //你输入的物品
+},
+"result": {//结果
+"amount": 10,//90mb
+"fluid": "tconstruct:molten_gold"//你定义的流体
+},
+"temperature": 800,//温度 800是常规燃料 1000是岩浆 1500是烈焰血
+"time": 8//时间 4是1秒
+}),
+)
+
+ServerEvents.recipes(event => 
+event.custom({//定义在炉子里融化
+"type": "tconstruct:melting",
+"ingredient": {//所需物品
+"item": "tfc:ore/normal_native_gold" //你输入的物品
+},
+"result": {//结果
+"amount": 25,//90mb
+"fluid": "tconstruct:molten_gold"//你定义的流体
+},
+"temperature": 800,//温度 800是常规燃料 1000是岩浆 1500是烈焰血
+"time": 20//时间 4是1秒
+}),
+)
+
+ServerEvents.recipes(event => 
+event.custom({//定义在炉子里融化
+"type": "tconstruct:melting",
+"ingredient": {//所需物品
+"item": "tfc:ore/rich_native_gold" //你输入的物品
+},
+"result": {//结果
+"amount": 35,//90mb
+"fluid": "tconstruct:molten_gold"//你定义的流体
+},
+"temperature": 800,//温度 800是常规燃料 1000是岩浆 1500是烈焰血
+"time": 28//时间 4是1秒
+}),
+)
+
+ServerEvents.recipes(event => 
+event.custom({//定义在炉子里融化
+"type": "tconstruct:melting",
+"ingredient": {//所需物品
+"item": "tfc:ore/poor_hematite" //你输入的物品
+},
+"result": {//结果
+"amount": 15,//90mb
+"fluid": "tconstruct:molten_iron"//你定义的流体
+},
+"temperature": 800,//温度 800是常规燃料 1000是岩浆 1500是烈焰血
+"time": 12//时间 4是1秒
+}),
+)
+
+ServerEvents.recipes(event => 
+event.custom({//定义在炉子里融化
+"type": "tconstruct:melting",
+"ingredient": {//所需物品
+"item": "tfc:ore/small_hematite" //你输入的物品
+},
+"result": {//结果
+"amount": 10,//90mb
+"fluid": "tconstruct:molten_iron"//你定义的流体
+},
+"temperature": 800,//温度 800是常规燃料 1000是岩浆 1500是烈焰血
+"time": 8//时间 4是1秒
+}),
+)
+
+ServerEvents.recipes(event => 
+event.custom({//定义在炉子里融化
+"type": "tconstruct:melting",
+"ingredient": {//所需物品
+"item": "tfc:ore/normal_hematite" //你输入的物品
+},
+"result": {//结果
+"amount": 25,//90mb
+"fluid": "tconstruct:molten_iron"//你定义的流体
+},
+"temperature": 800,//温度 800是常规燃料 1000是岩浆 1500是烈焰血
+"time": 20//时间 4是1秒
+}),
+)
+
+ServerEvents.recipes(event => 
+event.custom({//定义在炉子里融化
+"type": "tconstruct:melting",
+"ingredient": {//所需物品
+"item": "tfc:ore/rich_hematite" //你输入的物品
+},
+"result": {//结果
+"amount": 35,//90mb
+"fluid": "tconstruct:molten_iron"//你定义的流体
+},
+"temperature": 800,//温度 800是常规燃料 1000是岩浆 1500是烈焰血
+"time": 28//时间 4是1秒
+}),
+)
+
+ServerEvents.recipes(event => 
+event.custom({//定义在炉子里融化
+"type": "tconstruct:melting",
+"ingredient": {//所需物品
+"item": "tfc:ore/poor_native_silver" //你输入的物品
+},
+"result": {//结果
+"amount": 15,//90mb
+"fluid": "tconstruct:molten_silver"//你定义的流体
+},
+"temperature": 800,//温度 800是常规燃料 1000是岩浆 1500是烈焰血
+"time": 12//时间 4是1秒
+}),
+)
+
+ServerEvents.recipes(event => 
+event.custom({//定义在炉子里融化
+"type": "tconstruct:melting",
+"ingredient": {//所需物品
+"item": "tfc:ore/small_native_silver" //你输入的物品
+},
+"result": {//结果
+"amount": 10,//90mb
+"fluid": "tconstruct:molten_silver"//你定义的流体
+},
+"temperature": 800,//温度 800是常规燃料 1000是岩浆 1500是烈焰血
+"time": 8//时间 4是1秒
+}),
+)
+
+ServerEvents.recipes(event => 
+event.custom({//定义在炉子里融化
+"type": "tconstruct:melting",
+"ingredient": {//所需物品
+"item": "tfc:ore/normal_native_silver" //你输入的物品
+},
+"result": {//结果
+"amount": 25,//90mb
+"fluid": "tconstruct:molten_silver"//你定义的流体
+},
+"temperature": 800,//温度 800是常规燃料 1000是岩浆 1500是烈焰血
+"time": 20//时间 4是1秒
+}),
+)
+
+ServerEvents.recipes(event => 
+event.custom({//定义在炉子里融化
+"type": "tconstruct:melting",
+"ingredient": {//所需物品
+"item": "tfc:ore/rich_native_silver" //你输入的物品
+},
+"result": {//结果
+"amount": 35,//90mb
+"fluid": "tconstruct:molten_silver"//你定义的流体
+},
+"temperature": 800,//温度 800是常规燃料 1000是岩浆 1500是烈焰血
+"time": 28//时间 4是1秒
+}),
+)
+
+ServerEvents.recipes(event => 
+event.custom({//定义在炉子里融化
+"type": "tconstruct:melting",
+"ingredient": {//所需物品
+"item": "tfc:ore/small_cassiterite" //你输入的物品
+},
+"result": {//结果
+"amount": 10,//90mb
+"fluid": "tconstruct:molten_tin"//你定义的流体
+},
+"temperature": 800,//温度 800是常规燃料 1000是岩浆 1500是烈焰血
+"time": 8//时间 4是1秒
+}),
+)
+
+ServerEvents.recipes(event => 
+event.custom({//定义在炉子里融化
+"type": "tconstruct:melting",
+"ingredient": {//所需物品
+"item": "tfc:ore/normal_cassiterite" //你输入的物品
+},
+"result": {//结果
+"amount": 25,//90mb
+"fluid": "tconstruct:molten_tin"//你定义的流体
+},
+"temperature": 800,//温度 800是常规燃料 1000是岩浆 1500是烈焰血
+"time": 20//时间 4是1秒
+}),
+)
+
+ServerEvents.recipes(event => 
+event.custom({//定义在炉子里融化
+"type": "tconstruct:melting",
+"ingredient": {//所需物品
+"item": "tfc:ore/rich_cassiterite" //你输入的物品
+},
+"result": {//结果
+"amount": 35,//90mb
+"fluid": "tconstruct:molten_tin"//你定义的流体
+},
+"temperature": 800,//温度 800是常规燃料 1000是岩浆 1500是烈焰血
+"time": 28//时间 4是1秒
+}),
+)
+
+ServerEvents.recipes(event => 
+event.custom({//定义在炉子里融化
+"type": "tconstruct:melting",
+"ingredient": {//所需物品
+"item": "tfc:ore/poor_cassiterite" //你输入的物品
+},
+"result": {//结果
+"amount": 15,//90mb
+"fluid": "tconstruct:molten_tin"//你定义的流体
+},
+"temperature": 800,//温度 800是常规燃料 1000是岩浆 1500是烈焰血
+"time": 12//时间 4是1秒
+}),
+)
+
+ServerEvents.recipes(event => 
+event.custom({//定义在炉子里融化
+"type": "tconstruct:melting",
+"ingredient": {//所需物品
+"item": "tfc:ore/poor_malachite" //你输入的物品
+},
+"result": {//结果
+"amount": 15,//90mb
+"fluid": "tconstruct:molten_copper"//你定义的流体
+},
+"temperature": 800,//温度 800是常规燃料 1000是岩浆 1500是烈焰血
+"time": 24//时间 4是1秒
+}),
+)
+
+ServerEvents.recipes(event => 
+event.custom({//定义在炉子里融化
+"type": "tconstruct:melting",
+"ingredient": {//所需物品
+"item": "tfc:ore/rich_malachite" //你输入的物品
+},
+"result": {//结果
+"amount": 35,//90mb
+"fluid": "tconstruct:molten_copper"//你定义的流体
+},
+"temperature": 800,//温度 800是常规燃料 1000是岩浆 1500是烈焰血
+"time": 28//时间 4是1秒
+}),
+)
+
+ServerEvents.recipes(event => 
+event.custom({//定义在炉子里融化
+"type": "tconstruct:melting",
+"ingredient": {//所需物品
+"item": "tfc:ore/normal_malachite" //你输入的物品
+},
+"result": {//结果
+"amount": 25,//90mb
+"fluid": "tconstruct:molten_copper"//你定义的流体
+},
+"temperature": 800,//温度 800是常规燃料 1000是岩浆 1500是烈焰血
+"time": 20//时间 4是1秒
+}),
+)
+
+ServerEvents.recipes(event => 
+event.custom({//定义在炉子里融化
+"type": "tconstruct:melting",
+"ingredient": {//所需物品
+"item": "tfc:ore/small_malachite" //你输入的物品
+},
+"result": {//结果
+"amount": 10,//90mb
+"fluid": "tconstruct:molten_copper"//你定义的流体
+},
+"temperature": 800,//温度 800是常规燃料 1000是岩浆 1500是烈焰血
+"time": 8//时间 4是1秒
+}),
+)
+
+ServerEvents.recipes(event => 
+event.custom({//定义在炉子里融化
+"type": "tconstruct:melting",
+"ingredient": {//所需物品
+"item": "tfc:ore/poor_magnetite" //你输入的物品
+},
+"result": {//结果
+"amount": 15,//90mb
+"fluid": "tconstruct:molten_iron"//你定义的流体
+},
+"temperature": 800,//温度 800是常规燃料 1000是岩浆 1500是烈焰血
+"time": 12//时间 4是1秒
+}),
+)
+
+ServerEvents.recipes(event => 
+event.custom({//定义在炉子里融化
+"type": "tconstruct:melting",
+"ingredient": {//所需物品
+"item": "tfc:ore/small_magnetite" //你输入的物品
+},
+"result": {//结果
+"amount": 10,//90mb
+"fluid": "tconstruct:molten_iron"//你定义的流体
+},
+"temperature": 800,//温度 800是常规燃料 1000是岩浆 1500是烈焰血
+"time": 8//时间 4是1秒
+}),
+)
+
+ServerEvents.recipes(event => 
+event.custom({//定义在炉子里融化
+"type": "tconstruct:melting",
+"ingredient": {//所需物品
+"item": "tfc:ore/normal_magnetite" //你输入的物品
+},
+"result": {//结果
+"amount": 25,//90mb
+"fluid": "tconstruct:molten_iron"//你定义的流体
+},
+"temperature": 800,//温度 800是常规燃料 1000是岩浆 1500是烈焰血
+"time": 20//时间 4是1秒
+}),
+)
+
+ServerEvents.recipes(event => 
+event.custom({//定义在炉子里融化
+"type": "tconstruct:melting",
+"ingredient": {//所需物品
+"item": "tfc:ore/rich_magnetite" //你输入的物品
+},
+"result": {//结果
+"amount": 35,//90mb
+"fluid": "tconstruct:molten_iron"//你定义的流体
+},
+"temperature": 800,//温度 800是常规燃料 1000是岩浆 1500是烈焰血
+"time": 28//时间 4是1秒
+}),
+)
+
+ServerEvents.recipes(event => 
+event.custom({//定义在炉子里融化
+"type": "tconstruct:melting",
+"ingredient": {//所需物品
+"item": "tfc:ore/poor_limonite" //你输入的物品
+},
+"result": {//结果
+"amount": 15,//90mb
+"fluid": "tconstruct:molten_iron"//你定义的流体
+},
+"temperature": 800,//温度 800是常规燃料 1000是岩浆 1500是烈焰血
+"time": 12//时间 4是1秒
+}),
+)
+
+ServerEvents.recipes(event => 
+event.custom({//定义在炉子里融化
+"type": "tconstruct:melting",
+"ingredient": {//所需物品
+"item": "tfc:ore/small_limonite" //你输入的物品
+},
+"result": {//结果
+"amount": 10,//90mb
+"fluid": "tconstruct:molten_iron"//你定义的流体
+},
+"temperature": 800,//温度 800是常规燃料 1000是岩浆 1500是烈焰血
+"time": 8//时间 4是1秒
+}),
+)
+
+ServerEvents.recipes(event => 
+event.custom({//定义在炉子里融化
+"type": "tconstruct:melting",
+"ingredient": {//所需物品
+"item": "tfc:ore/normal_limonite" //你输入的物品
+},
+"result": {//结果
+"amount": 25,//90mb
+"fluid": "tconstruct:molten_iron"//你定义的流体
+},
+"temperature": 800,//温度 800是常规燃料 1000是岩浆 1500是烈焰血
+"time": 20//时间 4是1秒
+}),
+)
+
+ServerEvents.recipes(event => 
+event.custom({//定义在炉子里融化
+"type": "tconstruct:melting",
+"ingredient": {//所需物品
+"item": "tfc:ore/rich_limonite" //你输入的物品
+},
+"result": {//结果
+"amount": 35,//90mb
+"fluid": "tconstruct:molten_iron"//你定义的流体
+},
+"temperature": 800,//温度 800是常规燃料 1000是岩浆 1500是烈焰血
+"time": 28//时间 4是1秒
+}),
+)
+
+ServerEvents.recipes(event => 
+event.custom({//定义在炉子里融化
+"type": "tconstruct:melting",
+"ingredient": {//所需物品
+"item": "tfc:ore/poor_sphalerite" //你输入的物品
+},
+"result": {//结果
+"amount": 15,//90mb
+"fluid": "tconstruct:molten_zinc"//你定义的流体
+},
+"temperature": 800,//温度 800是常规燃料 1000是岩浆 1500是烈焰血
+"time": 12//时间 4是1秒
+}),
+)
+
+ServerEvents.recipes(event => 
+event.custom({//定义在炉子里融化
+"type": "tconstruct:melting",
+"ingredient": {//所需物品
+"item": "tfc:ore/small_sphalerite" //你输入的物品
+},
+"result": {//结果
+"amount": 10,//90mb
+"fluid": "tconstruct:molten_zinc"//你定义的流体
+},
+"temperature": 800,//温度 800是常规燃料 1000是岩浆 1500是烈焰血
+"time": 8//时间 4是1秒
+}),
+)
+
+ServerEvents.recipes(event => 
+event.custom({//定义在炉子里融化
+"type": "tconstruct:melting",
+"ingredient": {//所需物品
+"item": "tfc:ore/normal_sphalerite" //你输入的物品
+},
+"result": {//结果
+"amount": 25,//90mb
+"fluid": "tconstruct:molten_zinc"//你定义的流体
+},
+"temperature": 800,//温度 800是常规燃料 1000是岩浆 1500是烈焰血
+"time": 20//时间 4是1秒
+}),
+)
+
+ServerEvents.recipes(event => 
+event.custom({//定义在炉子里融化
+"type": "tconstruct:melting",
+"ingredient": {//所需物品
+"item": "tfc:ore/rich_sphalerite" //你输入的物品
+},
+"result": {//结果
+"amount": 35,//90mb
+"fluid": "tconstruct:molten_zinc"//你定义的流体
+},
+"temperature": 800,//温度 800是常规燃料 1000是岩浆 1500是烈焰血
+"time": 28//时间 4是1秒
+}),
+)
+
+ServerEvents.recipes(event => 
+event.custom({//定义在炉子里融化
+"type": "tconstruct:melting",
+"ingredient": {//所需物品
+"item": "tfc:ore/poor_tetrahedrite" //你输入的物品
+},
+"result": {//结果
+"amount": 15,//90mb
+"fluid": "tconstruct:molten_copper"//你定义的流体
+},
+"temperature": 800,//温度 800是常规燃料 1000是岩浆 1500是烈焰血
+"time": 12//时间 4是1秒
+}),
+)
+
+ServerEvents.recipes(event => 
+event.custom({//定义在炉子里融化
+"type": "tconstruct:melting",
+"ingredient": {//所需物品
+"item": "tfc:ore/small_tetrahedrite" //你输入的物品
+},
+"result": {//结果
+"amount": 10,//90mb
+"fluid": "tconstruct:molten_copper"//你定义的流体
+},
+"temperature": 800,//温度 800是常规燃料 1000是岩浆 1500是烈焰血
+"time": 8//时间 4是1秒
+}),
+)
+
+ServerEvents.recipes(event => 
+event.custom({//定义在炉子里融化
+"type": "tconstruct:melting",
+"ingredient": {//所需物品
+"item": "tfc:ore/normal_tetrahedrite" //你输入的物品
+},
+"result": {//结果
+"amount": 25,//90mb
+"fluid": "tconstruct:molten_copper"//你定义的流体
+},
+"temperature": 800,//温度 800是常规燃料 1000是岩浆 1500是烈焰血
+"time": 20//时间 4是1秒
+}),
+)
+
+ServerEvents.recipes(event => 
+event.custom({//定义在炉子里融化
+"type": "tconstruct:melting",
+"ingredient": {//所需物品
+"item": "tfc:ore/rich_tetrahedrite" //你输入的物品
+},
+"result": {//结果
+"amount": 35,//90mb
+"fluid": "tconstruct:molten_copper"//你定义的流体
+},
+"temperature": 800,//温度 800是常规燃料 1000是岩浆 1500是烈焰血
+"time": 28//时间 4是1秒
+}),
+)
